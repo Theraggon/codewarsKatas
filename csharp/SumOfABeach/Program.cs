0359a14b3eff31ee8b3013b6c4b8a5c84da1483b
Theraggon <Theraggon@gmail.com>
2019-07-30 11:18:36 +0200

    SumOfABeach Init

---
+++ csharp/SumOfABeach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumOfABeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
