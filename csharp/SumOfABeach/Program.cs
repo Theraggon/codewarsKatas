605797ac706741ead50805598e61282e30d2fc0d
Theraggon <Theraggon@gmail.com>
2019-07-31 13:00:39 +0200

    Working SumOfABeach Kata

--- csharp/SumOfABeach/Program.cs
+++ csharp/SumOfABeach/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace SumOfABeach
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+	public class Kata
+	{
+		private static string [] words = {"sand", "water", "fish", "sun"};
+
+		public static int SumOfABeach(string s)
+		{
+			var wordsSet = new SortedSet<string>(words);
+			s = s.ToLower();
+			int result = 0;
+			for (int startIndex = 0;startIndex < s.Length;++startIndex)
+			{
+				foreach (var word in wordsSet)
+				{
+					if (startIndex + word.Length <= s.Length
+					    && s.Substring(startIndex, word.Length).Contains(word.ToLower()))
+					{
+						++result;
+
+						// Length-1 because after break; for cycle ends with ++startIndex
+						startIndex += word.Length-1;
+						break;
+					}
+				}
+			}
+			return result;
+		}
+
+	}
+
+
 }
+
+
+
