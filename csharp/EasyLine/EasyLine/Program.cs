cb83e16a860510085d9bddffac82b5bfa01deb17
Theraggon <Theraggon@gmail.com>
2019-08-05 19:46:17 +0200

    Easy Line kata code

---
+++ csharp/EasyLine/EasyLine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace EasyLineNS
+{
+    class EasyLines
+    {
+
+        public static BigInteger EasyLine(int n)
+        {
+            BigInteger [] row = new BigInteger[n + 1];
+            row[0] = 1;
+            row[1] = 1;
+
+            BigInteger x;
+            for (int i = 0; i < (int)(n/2);++i)
+            {
+                x = row[i] * (n - i) / (i + 1);
+                row[i + 1] = x;
+                row[n - i - 1] = x;
+            }
+            BigInteger result = 0;
+            foreach(BigInteger number in row)
+            {
+                result += number * number;
+            }
+
+            return result + 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EasyLine(13));
+        }
+    }
+}
